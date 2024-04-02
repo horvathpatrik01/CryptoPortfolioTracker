76c7e3051248563b403bbafd70ac02f6ccfdd74d
hpatrik10 <hpatrik10@gmail.com>
2024-04-02 17:38:47 +0200

    Create starting folders

---
+++ Database/DbContext/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Database.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database.DbContext
+{
+    public class AppDbContext : IdentityDbContext<User, IdentityRole<int>, int>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Database/Migrations/20240503105759_PortfolioEntityChanged.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class PortfolioEntityChanged : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Portfolios",
+                type: "nvarchar(32)",
+                maxLength: 32,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AddColumn<int>(
+                name: "IconColor",
+                table: "Portfolios",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CexIdentifier",
+                table: "ApiKeys",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IconColor",
+                table: "Portfolios");
+
+            migrationBuilder.DropColumn(
+                name: "CexIdentifier",
+                table: "ApiKeys");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Portfolios",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(32)",
+                oldMaxLength: 32);
+        }
+    }
+}
