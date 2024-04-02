8f882e74390e1e7583112d010eab0bdc713385bd
hpatrik10 <hpatrik10@gmail.com>
2024-04-02 17:38:47 +0200

    Create starting folders

---
+++ Database/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Database.Entities
+{
+    public class User : IdentityUser<int>
+    {
+        [MaxLength(30)]
+        public string? FirstName { get; set; }
+        [MaxLength(30)]
+        public string? LastName { get; set; }
+        public DateTime CreatedTimeStamp { get; set; }
+
+        // Navigation properties
+    }
+}
