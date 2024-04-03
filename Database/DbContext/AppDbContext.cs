5a71aba46d003c9b0d941730d02182f50748aca5
M00NBAG <berkybencetibor@gmail.com>
2024-04-03 13:18:45 +0200

    debugged entities and AppDbContext.cs

--- Database/DbContext/AppDbContext.cs
+++ Database/DbContext/AppDbContext.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace Database.DbContext
+namespace Database.DatabaseContext
 {
     public class AppDbContext : DbContext
     {
