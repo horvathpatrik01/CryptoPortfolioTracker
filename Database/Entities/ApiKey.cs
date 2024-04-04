b589a5568267e0fd56e260859b1af1050e4b94c1
Horváth Patrik <hpatrik10@gmail.hu>
2024-04-04 12:02:01 +0200

    Database design created

---
+++ Database/Entities/ApiKey.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Entities
+{
+    [PrimaryKey("PublicKey")]
+    public class ApiKey
+    {
+        public string PublicKey { get; set; }
+        public string PrivateKey { get; set; }
+
+        [ForeignKey(nameof(PortfolioId))]
+        public int PortfolioId { get; set; }
+
+        // Navigation property
+        public Portfolio Portfolio { get; set; } = null!;
+    }
+}
