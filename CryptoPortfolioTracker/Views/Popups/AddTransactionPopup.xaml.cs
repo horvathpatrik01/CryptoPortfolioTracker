4c8f4fdb0e38f860da9081fcfabb263ce3856209
M00NBAG <berkybencetibor@gmail.com>
2024-05-09 17:48:14 +0200

    - Edited button styles - fixed AddTransaction type selection design - fixed transaction collection view design

--- CryptoPortfolioTracker/Views/Popups/AddTransactionPopup.xaml.cs
+++ CryptoPortfolioTracker/Views/Popups/AddTransactionPopup.xaml.cs
@@ -28,30 +28,22 @@ public partial class AddTransactionPopup : Popup
     {
         // Show the selected view
         var button = (Button)sender;
-        Color lightPrimaryColor = (Color)App.Current?.Resources["LightPrimary"];
-        Color lightSecondaryColor = (Color)App.Current.Resources["LightSecondary"];
-        Color darkPrimaryColor = (Color)App.Current.Resources["DarkPrimary"];
-        Color darkSecondaryColor = (Color)App.Current.Resources["DarkSecondary"];
-        switch (button.Text)
+        Color color, selectedColor;
+        if (App.Current?.UserAppTheme == AppTheme.Light)
         {
-            case "Buy":
-                PPCView.IsVisible = true;
-                sellBtn.TextColor = App.Current?.UserAppTheme == AppTheme.Light ? lightSecondaryColor : darkPrimaryColor;
-                transferBtn.TextColor = App.Current?.UserAppTheme == AppTheme.Light ? lightSecondaryColor : darkPrimaryColor;
-                break;
-
-            case "Sell":
-                PPCView.IsVisible = true;
-                buyBtn.TextColor = App.Current?.UserAppTheme == AppTheme.Light ? lightSecondaryColor : darkPrimaryColor;
-                transferBtn.TextColor = App.Current?.UserAppTheme == AppTheme.Light ? lightSecondaryColor : darkPrimaryColor;
-                break;
-
-            case "Transfer":
-                PPCView.IsVisible = false;
-                sellBtn.TextColor = App.Current?.UserAppTheme == AppTheme.Light ? lightSecondaryColor : darkPrimaryColor;
-                buyBtn.TextColor = App.Current?.UserAppTheme == AppTheme.Light ? lightSecondaryColor : darkPrimaryColor;
-                break;
+            color = (Color)App.Current?.Resources["LightSecondary"];
+            selectedColor = (Color)App.Current?.Resources["LightPrimary"];
         }
-        button.TextColor = App.Current?.UserAppTheme == AppTheme.Light ? lightPrimaryColor : darkSecondaryColor;
+        else
+        {
+            color = (Color)App.Current?.Resources["DarkPrimary"];
+            selectedColor = (Color)App.Current?.Resources["DarkSecondary"];
+        }
+        buyBtn.BackgroundColor = color;
+        sellBtn.BackgroundColor = color;
+        transferBtn.BackgroundColor = color;
+        button.BackgroundColor = selectedColor;
+        PPCView.IsVisible = (!(button.Text == "Transfer"));
+
     }
 }
