6126f6a8781178d1d923e994a497fc26577cf8d4
M00NBAG <berkybencetibor@gmail.com>
2024-05-20 13:48:37 +0200

    - Font changed - transaction selector style fixed - fixed activityIndicator style

--- CryptoPortfolioTracker/Views/Popups/AddTransactionPopup.xaml.cs
+++ CryptoPortfolioTracker/Views/Popups/AddTransactionPopup.xaml.cs
@@ -1,7 +1,8 @@
 using CommunityToolkit.Maui.Views;
 using CryptoPortfolioTracker.ViewModels;
-
+using Microsoft.Maui.Controls;
 using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace CryptoPortfolioTracker.Views.Popups;
 
@@ -9,11 +10,25 @@ public partial class AddTransactionPopup : Popup
 {
     private readonly TransactionViewModel _transactionViewModel;
 
+    private Color color, selectedColor;
+
     public AddTransactionPopup(TransactionViewModel transactionViewModel)
     {
         BindingContext = transactionViewModel;
         InitializeComponent();
         this._transactionViewModel = transactionViewModel;
+
+        if (App.Current?.UserAppTheme == AppTheme.Light)
+        {
+            color = (Color)App.Current?.Resources["LightSecondary"];
+            selectedColor = (Color)App.Current?.Resources["LightPrimary"];
+        }
+        else
+        {
+            color = (Color)App.Current?.Resources["DarkPrimary"];
+            selectedColor = (Color)App.Current?.Resources["DarkSecondary"];
+        }
+        buyBtn.BackgroundColor = selectedColor;
     }
 
     private async void Close(object? sender, EventArgs e)
@@ -24,11 +39,10 @@ public partial class AddTransactionPopup : Popup
         cts.Dispose();
     }
 
-    private void OnTransactionTypeChanged(object sender, EventArgs e)
+    private async void OnTransactionTypeChanged(object sender, EventArgs e)
     {
         // Show the selected view
         var button = (Button)sender;
-        Color color, selectedColor;
         if (App.Current?.UserAppTheme == AppTheme.Light)
         {
             color = (Color)App.Current?.Resources["LightSecondary"];
@@ -43,7 +57,24 @@ public partial class AddTransactionPopup : Popup
         sellBtn.BackgroundColor = color;
         transferBtn.BackgroundColor = color;
         button.BackgroundColor = selectedColor;
-        PPCView.IsVisible = (!(button.Text == "Transfer"));
-
+        if ((button.Text == "Transfer") && (PPCView.IsVisible))
+        {
+            await Task.WhenAny<bool>
+            (
+                PPCView.TranslateTo(0, 15, 250, Easing.CubicIn),
+                PPCView.FadeTo(0, 300)
+            );
+            PPCView.IsVisible = false;
+        }
+        else if (!(PPCView.IsVisible))
+        {
+            PPCView.IsVisible = true;
+            PPCView.TranslationY = -15;
+            await Task.WhenAny<bool>
+            (
+                PPCView.TranslateTo(0, 0, 250, Easing.CubicOut),
+                PPCView.FadeTo(1, 300)
+            );
+        }
     }
 }
